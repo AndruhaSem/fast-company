1b500fcad62a29247641b6e4539d6c379723d44f
Андрей Семин <glazunov.99@list.ru>
2022-09-15 16:35:34 +0300

    correct implementation  of the course project

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -9,6 +9,8 @@ const Users = () => {
     };
 
     const renderPhrase = (number) => {
+        const num = document.querySelector('.btn'); 
+        console.log(num)
         const lastOne = Number(number.toString().slice(-1));
         if (number > 4 && number < 15) return "человек тусанет";
         if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
